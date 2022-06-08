11ae0c43f57ff88f644d9adb64f4e9b2bfcd1442
Roni Noone <38510282+RoniNoone@users.noreply.github.com>
2022-06-08 14:47:18 -0400

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+/*First C program ever! So exciting*/
+int main(void)
+{
+    string userName = get_string("Hey Buddy! What's your name? ");
+    //formated a bit of space around my output
+    printf("\nHello, %s!\nNice job on your first C program!\n\n", userName);
+}
+
+//Only one question so far... my text editor is not color coding. Not sure why. Will change theme to test. 
